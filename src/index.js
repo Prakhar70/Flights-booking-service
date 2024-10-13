8404af2c6b2bd524313ccfabde2e964b39560f71
Prakhar70 <42569845+Prakhar70@users.noreply.github.com>
2024-10-13 16:24:45 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
+const logger = require("./config/logger-config");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  logger.info("Called form listen");
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+});
---
+++ src/utils/common/success-response.js
@@ -0,0 +1,8 @@
+const error = {
+  sucess: true,
+  message: "Something completed the request",
+  data: {},
+  error: {},
+};
+
+module.exports = error;
